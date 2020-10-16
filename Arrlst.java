6bd30af8680acbd0efbf5c886dd2fe27c7bd2f10
Hruturaj_Kedar <hruturaj.kedar14@gmail.com>
2020-10-16 19:12:08 +0530

    first file commited

---
+++ Arrlst.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public  class Arrlst
+{
+    public static void main(String[] args)
+    {
+    //  ArrayList fruit=new ArrayList<>();
+    //  fruit.add("Apple");
+    //  fruit.add(3);
+    //  System.out.println(fruit);
+    Pair<String,Integer> p1=new Pair("anuj",1);
+    p1.getDescription();
+        
+    }
+}
